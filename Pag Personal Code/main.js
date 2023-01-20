4c90bd5e50b0e103622aa656ae2eb13085d57bf6
claudiaGHB <122397115+claudiaGHB@users.noreply.github.com>
2023-01-20 00:48:24 -0600

    Add files via upload

--- Pag Personal Code/main.js
+++ Pag Personal Code/main.js
@@ -2,7 +2,7 @@ const container = document.querySelector('.fcontainer')
 const colors = ['#95E5AC', '#23D1CD', '#0D6A76', '#063A51']
 
 const figures = () => {
-    for(let i = 0; i <= 25; i++){
+    for(let i = 0; i <= 50; i++){
         let figure = document.createElement('span')
         let select = Math.round(colors.length * Math.random())
 
@@ -15,8 +15,45 @@ const figures = () => {
         setInterval(() => {
             figure.style.top = innerHeight * Math.random() + 'px'
             figure.style.left = innerWidth * Math.random() + 'px'
-        }, 5000)
+        }, 3000)
     }
 }
 
 figures()
+
+
+
+
+
+
+
+const carrusel = document.querySelector(".carrusel-items");
+
+let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
+let intervalo = null;
+let step = 1;
+const start = () => {
+  intervalo = setInterval(function () {
+    carrusel.scrollLeft = carrusel.scrollLeft + step;
+    if (carrusel.scrollLeft === maxScrollLeft) {
+      step = step * -1;
+    } else if (carrusel.scrollLeft===0) {
+      step = step * -1;
+    }
+  },20);
+};
+
+const stop = () => {
+  clearInterval(intervalo);
+};
+
+carrusel.addEventListener("mouseover", () => {
+  stop();
+});
+
+carrusel.addEventListener("mouseout", () => {
+  start();
+});
+
+start();
+
